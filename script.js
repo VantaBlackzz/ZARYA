3e3d7c5f0496b337961c69484ce192a5c2bb207f
VantaBlackzz <Vantablackzz@yandex.ru>
2022-08-13 21:35:41 +0400

    clearing code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let slideIndex = 0;
-  showSlides();
+	let slideIndex = 0;
+		showSlides();
 
   function showSlides() {
-    let i;
+	let i;
     let slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
