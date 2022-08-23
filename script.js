67ceff4eb726dc081a4e4a6685f71b705900102c
VantaBlack <Vantablackzz@yandex.ru>
2022-08-24 01:14:11 +0400

    adaptation_index_catalog_header

--- script.js
+++ script.js
@@ -37,3 +37,19 @@ function openSM() {
 function closeSM() {
 	document.getElementById("SM").style.width = "0%";
 };
+//Dropdown menu
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
